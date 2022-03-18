ae23be4a1ee138648d88754a42c60ebed62e774e
임현석 <prlhspt@gmail.com>
2022-03-18 19:16:17 +0900

    BFS 추가

--- java/BFS/EscapeMaze.java
+++ java/BFS/EscapeMaze.java
@@ -1,4 +1,96 @@
 package BFS;
 
+import java.util.*;
+
+/**
+5 6
+101010
+111111
+000001
+111111
+111111
+*/
+
+class Node {
+
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
 public class EscapeMaze {
+    static int n, m;
+    static int[][] graph = new int[201][201];
+
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
+
+    /**
+     * (0,0) 에서 시작
+     * 큐에다가 (-1,0)
+     * (1,0)
+     * (0,-1)
+     * (0,1)
+     * 을 넣고 그 값을 +1 하는 느낌?
+     */
+
+    static int bfs(int x, int y) {
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y));
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            x = node.getX();
+            y = node.getY();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                if (graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    q.offer(new Node(nx, ny));
+                }
+            }
+        }
+
+        return graph[n - 1][m - 1];
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+
+    }
+
 }
